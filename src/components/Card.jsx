dd06da792217acc283010c8b366e536bc8f738f5
Piggi98 <pvitale@studenti.laziodigital.it>
2024-05-27 15:37:21 +0200

    ciao

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card(){
+    return (
+        <div className="rounded-md bg-zinc-950">
+            <img 
+            src="https://unsplash.com/it/foto/fotografia-aerea-di-grandi-specchi-dacqua-e-costa-okVXy9tG3KY" 
+            alt=""></img>
+            <div className= "flex flex-col p-4">
+            <h2>Titolo</h2>
+            <p className="text-gray-500"> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+         </div>
+        </div>
+    )
+}
