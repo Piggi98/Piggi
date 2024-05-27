f44170eb1715dad0e7bcc673076f34115be53e86
Piggi98 <pvitale@studenti.laziodigital.it>
2024-05-27 15:52:40 +0200

    aggiunta spiaggia

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,13 @@
-function Card(){
+export default function Card() {
     return (
         <div className="rounded-md bg-zinc-950">
-            <img 
-            src="https://unsplash.com/it/foto/fotografia-aerea-di-grandi-specchi-dacqua-e-costa-okVXy9tG3KY" 
-            alt=""></img>
-            <div className= "flex flex-col p-4">
-            <h2>Titolo</h2>
-            <p className="text-gray-500"> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-         </div>
+            <img
+                src="https://images.unsplash.com/photo-1505118380757-91f5f5632de0?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""></img>
+            <div className="flex flex-col p-4">
+                <h2>Titolo</h2>
+                <p className="text-gray-500"> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
         </div>
     )
 }
